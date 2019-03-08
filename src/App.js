ad10162700a8e1f87e06cb6da298a9df5e6e0b8e
Nick Krekow <nkrekow@gmail.com>
2019-03-08 10:10:31 -0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Row, Button } from 'antd';
+import { Layout, Menu, Row, Button, Col } from 'antd';
+import { Route, Switch, Link, Redirect, NavLink} from 'react-router-dom';
 import "antd/dist/antd.css";
-// import './App.css';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.props = {
+    this.state = {
       data: []
     };
   }
@@ -22,10 +23,13 @@ class App extends Component {
   
   render() {
     return (
-      <Layout>
+      <Layout style={{height:'100vh'}}>
         <NavMenu />
-        <BodyContent />
-        <FooterContent />
+        {/* <Switch> */}
+          <Route exact path='/' component={BodyContent}/>
+          <Route path='/test' component={FooterContent}/>
+        {/* </Switch> */}
+        {/* <FooterContent /> */}
       </Layout>
     );
   }
@@ -44,7 +48,7 @@ class NavMenu extends Component {
           defaultSelectedKeys={['0']}
           style={{ lineHeight: '64px' }}
         >
-          <Menu.Item key="1">Foodfluence</Menu.Item>
+          <Menu.Item key="1"><Link to='/'>Foodfluence</Link></Menu.Item>
         </Menu>
       </Header>
     );
@@ -56,17 +60,20 @@ class BodyContent extends Component {
     const {Content} = Layout;
 
     return (
-      <Content style={{ padding: '20px 20px' }}>
-        <div style={{ background: '#fff', padding: '24px', height: '100vh' }}>
-          <div style={{textAlign:'center'}}>
-            <Row></Row>
-            <Row align='middle' justify='center' type='flex' style={{height:'100vh'}}>
-              <Row>
-                <h1>Foodfluence</h1>
-              </Row>
-              <Row>
-                <Button type='default'>Click me</Button>
-              </Row>
+      <Content style={{ padding: '20px 20px'}}>
+        <div style={{ background: '#fff', padding: '24px', height: '100%' }}>
+          <div style={{height:'100%'}}>
+            <Row align='middle' justify='center' type='flex' style={{height: '100%'}}>
+              <Col>
+                <Row type='flex' align='middle' justify='center'>
+                  <h1>foodfluence.</h1>
+                </Row>
+                <Row type='flex' align='middle' justify='center'>
+                  <Link to='/test'>
+                    <Button type='primary'>Click me</Button>
+                  </Link>
+                </Row>
+              </Col>
             </Row>
             {/* <Row>
             </Row> */}
