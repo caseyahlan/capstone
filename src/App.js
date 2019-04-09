0a8551c81de3da40e71df63e3b638143e7e562f4
Nick Krekow <nkrekow@gmail.com>
2019-04-08 20:08:38 -0700

    started over on first story

--- src/App.js
+++ src/App.js
@@ -1,104 +1,135 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Row, Button, Col } from 'antd';
-import { Route, Link, Redirect, NavLink} from 'react-router-dom';
-import InputBox from './Components/InputBox';
-import Results from './Components/Results';
-import "antd/dist/antd.css";
-import FoodForm from './FoodForm.js';
-import './App.css';
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
+import { Spring } from 'react-spring/renderprops';
+// import { Jumbotron } from 'react-bootstrap/Jumbotron';
+
+// const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
+const Pink = ({ children }) => <span style={{ color: '#FF6AC1' }}>{children}</span>
+const Yellow = ({ children }) => <span style={{ color: '#EFF59B' }}>{children}</span>
+const Lightblue = ({ children }) => <span style={{ color: '#9AEDFE' }}>{children}</span>
+const Green = ({ children }) => <span style={{ color: '#57EE89' }}>{children}</span>
+const Blue = ({ children }) => <span style={{ color: '#57C7FF' }}>{children}</span>
+const Gray = ({ children }) => <span style={{ color: '#909090' }}>{children}</span>
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-    };
-  }
-  
-  componentDidMount() {
-    fetch('./data/data.json')
-      .then((response) => (response.json()))
-      .then((data) => {
-        this.setState({data: data});
-      })
-      .catch((e) => {console.log(e)});
-  }
-  
   render() {
     return (
-      <Layout style={{height:'100vh'}}>
-        <NavMenu />
-        {/* <Switch> */}
-          <Route exact path='/' component={BodyContent}/>
-          <Route path='/input' render={(routerProps) => (
-            <InputBox {...routerProps} data={this.state.data}/>
-          )}/>
-          <Route path='/results' component={Results}/>
-        {/* </Switch> */}
-        {/* <FooterContent /> */}
-      </Layout>
-    );
-  }
-}
+      <Parallax ref={ref => (this.parallax = ref)} pages={9}>
+        <ParallaxLayer offset={0} speed={1} style={{ backgroundColor: '#4454E0' }} />
+        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#87BCDE' }} />
 
-class NavMenu extends Component {
-  render() {
-    const {Header} = Layout;
+        <ParallaxLayer offset={2} speed={1} factor={5}>
+          <h1>IMPACTS OF FISHING</h1>
+        </ParallaxLayer>
 
-    return (
-      <Header style={{width:'100%'}}>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['0']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key="1"><Link to='/'>Foodfluence</Link></Menu.Item>
-        </Menu>
-      </Header>
-    );
-  }
-}
+        <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
+          <img src='https://cdn11.bigcommerce.com/s-hii7479o/images/stencil/original/products/10545/26845/fish_1__56306.1526673301.png?c=2' style={{ width: '15%', marginLeft: '70%' }} />
+        </ParallaxLayer>
 
-class BodyContent extends Component {
-  render() {
-    const {Content} = Layout;
+        {/* <ParallaxLayer offset={2.5} speed={-0.4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
+          <img src='http://pluspng.com/img-png/circle-png-circle-png-hd-1600.png' style={{ width: '20%' }} />
+        </ParallaxLayer> */}
 
-    return (
-      <Content style={{ padding: '20px 20px'}}>
-        <div style={{ background: '#fff', padding: '24px', height: '100%' }}>
-          <div style={{height:'100%'}}>
-            <Row align='middle' justify='center' type='flex' style={{height: '100%'}}>
-              <Col>
-                <Row type='flex' align='middle' justify='center'>
-                  <h1>foodfluence.</h1>
-                </Row>
-                <Row type='flex' align='middle' justify='center'>
-                  <Link to='/input'>
-                    <Button type='primary'>Enter</Button>
-                  </Link>
-                </Row>
-              </Col>
-            </Row>
-          </div>
-        </div>
-      </Content>
-    );
-  }
-}
+        {/* <ParallaxLayer offset={2} speed={0.4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'left', pointerEvents: 'none' }}>
+          <img src='http://pluspng.com/img-png/circle-png-circle-png-hd-1600.png' style={{ width: '20%' }} />
+        </ParallaxLayer>
 
-class FooterContent extends Component {
-  render() {
-    const {Footer} = Layout;
+        <ParallaxLayer offset={2} speed={0.4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'right', pointerEvents: 'none' }}>
+          <img src='http://pluspng.com/img-png/circle-png-circle-png-hd-1600.png' style={{ width: '20%' }} />
+        </ParallaxLayer> */}
 
-    return (
-      <Footer>
-        some footer text
-      </Footer>
+        {/* <ParallaxLayer
+          offset={2}
+          speed={-0.3}
+          style={{
+            backgroundSize: '80%',
+            backgroundPosition: 'center',
+            backgroundImage: 'http://pluspng.com/img-png/circle-png-circle-png-hd-1600.png'
+          }}
+        /> */}
+
+        <ParallaxLayer
+          offset={0}
+          speed={0.1}
+          onClick={() => this.parallax.scrollTo(1)}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Spring
+            from={{ opacity: 0 }}
+            to={{opacity: 1 }}>
+            {props => <h1 style={props}>FISH</h1>}
+          </Spring>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1}
+          speed={0.1}
+          onClick={() => this.parallax.scrollTo(2)}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Spring
+            from={{ opacity: 0 }}
+            to={{ opacity: 1 }}>
+            {props => <p style={props}>Research, however, has indicated that if the fishing industry continues as is, wild-caught seafood will be non-existent by the year 2048 - worldatlas.com</p>}
+          </Spring>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={0.1}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          onClick={() => this.parallax.scrollTo(3)}>
+          <img src='https://vectr.com/tmp/a19mOuHvgF/b2RJ0682uA.svg?width=451.03&height=250&select=fJycNYaHz,a4eEByws9B,g3bdeiGle,aKh5hjDVk,e7IidWsg1,eDagJRHhR,b5YMasxcQf,g1nKjucdzP&source=selection' style={{ width: '60%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={0.1}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          onClick={() => this.parallax.scrollTo(4)}>
+          <img src='https://cdn11.bigcommerce.com/s-hii7479o/images/stencil/original/products/10545/26845/fish_1__56306.1526673301.png?c=2' style={{ width: '20%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={4}
+          speed={0.1}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          onClick={() => this.parallax.scrollTo(5)}>
+          <img src='https://cdn11.bigcommerce.com/s-hii7479o/images/stencil/original/products/10545/26845/fish_1__56306.1526673301.png?c=2' style={{ width: '20%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={5}
+          speed={0.1}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          onClick={() => this.parallax.scrollTo(6)}>
+          <img src='https://cdn11.bigcommerce.com/s-hii7479o/images/stencil/original/products/10545/26845/fish_1__56306.1526673301.png?c=2' style={{ width: '20%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={6}
+          speed={0.1}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          onClick={() => this.parallax.scrollTo(7)}>
+          <img src='https://cdn11.bigcommerce.com/s-hii7479o/images/stencil/original/products/10545/26845/fish_1__56306.1526673301.png?c=2' style={{ width: '20%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={7}
+          speed={0.1}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          onClick={() => this.parallax.scrollTo(8)}>
+          <img src='https://cdn11.bigcommerce.com/s-hii7479o/images/stencil/original/products/10545/26845/fish_1__56306.1526673301.png?c=2' style={{ width: '20%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={8}
+          speed={-0}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          onClick={() => this.parallax.scrollTo(0)}>
+          <img src='https://cdn11.bigcommerce.com/s-hii7479o/images/stencil/original/products/10545/26845/fish_1__56306.1526673301.png?c=2' style={{ width: '20%' }} />
+        </ParallaxLayer>
+      </Parallax>
     )
   }
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,23 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import firebase from 'firebase/app';
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCHgIzE13SFnPENyEsVi8ZxLjuFzVjVxWI",
-    authDomain: "foodfluence.firebaseapp.com",
-    databaseURL: "https://foodfluence.firebaseio.com",
-    projectId: "foodfluence",
-    storageBucket: "foodfluence.appspot.com",
-    messagingSenderId: "345768678571"
-};
-firebase.initializeApp(config);
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
