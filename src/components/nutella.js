227a0e13374c94c8e0a45380dd23f463e93e4e05
Nick Krekow <nkrekow@gmail.com>
2019-04-25 19:44:50 -0700

    cleaned up files & code

--- src/components/nutella.js
+++ src/components/nutella.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 import { Spring } from 'react-spring/renderprops';
 import { Container, Row, Col, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
@@ -8,58 +7,79 @@ class Nutella extends Component {
       return (
           <Parallax ref={ref => (this.parallax = ref)} pages={7}>
             
-            {/* Background colors for each slide */}
-            {/* <ParallaxLayer offset={0} speed={1} className='pink-background'/> */}
+            {/* Slide One */}
+            <ParallaxLayer
+              offset={0}
+              speed={0.1}
+              onClick={() => this.parallax.scrollTo(1)}>
+              <Container fluid={true}>
+                <Row>
+                  <Col xs='12' className='split-horizontal pink-background d-flex justify-content-center align-items-end'>
+                    <h1 className='text-white pb-3'>Nutella</h1>
+                  </Col>
+                  <Col xs='12' className='d-flex justify-content-center'>
+                    <div className='arrow-down'></div>
+                  </Col>
+                  <Col xs='12' className='split-horizontal d-flex justify-content-center'>
+                    <h3 className='pt-5'>Know Your Impact</h3>
+                  </Col>
+                </Row>
+              </Container>
+            </ParallaxLayer>
+            
+            {/* Slide Two */}
             <ParallaxLayer offset={1} speed={1} className='grey-background'/>
-            <ParallaxLayer offset={2} speed={1} />
+            <ParallaxLayer offset={1} speed={0.1}>
+              <Container>
+                <h1 className='text-center text-md-left pt-5'>About Nutella</h1>
+                <Row className=''>
+                  <Col sm='12' md='5'>
+                    <p className='text-monospace'>
+                      Nutella is a popular hazelnut spread that is commonly used in many food products. Currently, there are 7
+                      well known ingredients: sugar, palm oil, hazelnuts, cocoa, milk, soy, and vanilla (Citation 1). 
+                    </p>
+                  </Col>
+                </Row>
+                <Row className=''>
+                  <Col sm='12' md='5'>
+                    <p className='text-monospace'>
+                      Just by looking at the ingredients, nutella seems like a perfect choice for many recipes. However, nutella’s
+                      heavy use of palm oil spreads damage to the environment. 
+                    </p>
+                  </Col>
+                </Row>
+              </Container>
+            </ParallaxLayer>
 
-            {/* Titles for each slide */}
-            {/* <ParallaxLayer offset={1.05} speed={1.5} style={{ pointerEvents: 'none' }}>
-              <h1 className='text-center'>About Nutella</h1>
-            </ParallaxLayer> */}
-    
+            {/* Slide Three */}
             <ParallaxLayer offset={2.05} speed={1.5} style={{ pointerEvents: 'none' }}>
               <h1 style={{ width: '70%', marginLeft: '5%' }}>Impact of Palm Oil</h1>
             </ParallaxLayer>
-    
-            {/* Content for each slide */}
-            <ParallaxLayer offset={1} speed={0.1} style={{ }}>
-              <h1 className='text-sm-center text-md-left px-md-5 pt-md-5'>About Nutella</h1>
-              <Row className='px-md-5'>
-                <Col sm='12' md='5'>
-                  <p className='text-monospace'>
-                    Nutella is a popular hazelnut spread that is commonly used in many food products. Currently, there are 7 well known ingredients: sugar, palm oil, hazelnuts, cocoa, milk, soy, and vanilla (Citation 1). 
-                  </p>
-                </Col>
-              </Row>
-              <Row className='px-md-5'>
-                <Col sm='12' md='5'>
-                  <p className='text-monospace'>
-                    Just by looking at the ingredients, nutella seems like a perfect choice for many recipes. However, nutella’s heavy use of palm oil spreads damage to the environment. 
-                  </p>
-                </Col>
-              </Row>
-            </ParallaxLayer>
-            
-            <ParallaxLayer offset={2} speed={0.1} style={{ }}>
+            <ParallaxLayer offset={2} speed={0.1}>
               <h1>palm circles go here</h1>
             </ParallaxLayer>
 
-            <ParallaxLayer offset={3} speed={1} style={{ }}>
-              <h1 className='text-sm-center text-md-left px-md-5 pt-md-5'>How has Nutella Impacted Orangutans?</h1>
-              <Row className='px-md-5'>
-                <Col sm='12' md='5'>
-                  <p className='text-monospace font-italic'>
-                  “Over 50,000 orangutans on the islands of Borneo and Sumatra have died because of palm oil deforestation. Orangutans whose habitats have been destroyed often enter villages and oil plantations 
-                  in search of food where they are captured or killed by farmers who treat them as pests.  In 2016, it was reported that just 45,000 orangutans remained in Borneo and at this rate, 
-                  they will be extinct in the wild in just 25 years”. -orangutantrekkingtours.com
-                  </p>
-                </Col>
-              </Row>
+            {/* Slide Four */}
+            <ParallaxLayer offset={3} speed={1}>
+              <Container>
+                <h1 className='text-sm-center text-md-left px-md-5 pt-md-5'>How has Nutella Impacted Orangutans?</h1>
+                <Row className='px-md-5'>
+                  <Col sm='12' md='5'>
+                      <p className='text-monospace font-italic'>
+                        “Over 50,000 orangutans on the islands of Borneo and Sumatra have died because of palm oil deforestation. 
+                        Orangutans whose habitats have been destroyed often enter villages and oil plantations in search of food
+                        where they are captured or killed by farmers who treat them as pests.  In 2016, it was reported that just
+                        45,000 orangutans remained in Borneo and at this rate, they will be extinct in the wild in just 25 years”.
+                        -orangutantrekkingtours.com
+                      </p>
+                  </Col>
+                </Row>
+              </Container>
             </ParallaxLayer>
 
+            {/* Slide Five */}
             <ParallaxLayer offset={4} speed={0.5}>
-              <Container fluid='true'>
+              <Container fluid={true}>
                 <Row>
                   <Col md='6' className='split-vertical pink-background text-white d-flex align-items-center flex-wrap'>
                     <Row>
@@ -68,27 +88,30 @@ class Nutella extends Component {
                     </Row>
                   </Col>
                   <Col md='6' className='split-vertical'>
-                    <ListGroup>
-                      <ListGroupItem>
-                        <ListGroupItemHeading>Nutella</ListGroupItemHeading>
-                        <ListGroupItemText>Try Nicciolata Organic Hazelnut Spread with Cocoa & Milk; this product is also GMO Free</ListGroupItemText>
-                      </ListGroupItem>
-                      <ListGroupItem>
-                        <ListGroupItemHeading>Clif Bar</ListGroupItemHeading>
-                        <ListGroupItemText>Try Health Warriors Chia Bar; this product is 100 calories and has 3g of sugar</ListGroupItemText>
-                      </ListGroupItem>
-                      <ListGroupItem>
-                        <ListGroupItemHeading>JIF Peanut Butter</ListGroupItemHeading>
-                        <ListGroupItemText>Try Wild Friends Foods Chocolate Coconut Peanut Butter; this product is kosher and gluten-free</ListGroupItemText>
-                      </ListGroupItem>
-                    </ListGroup>
+                    <Container>
+                      <ListGroup>
+                        <ListGroupItem>
+                          <ListGroupItemHeading>Nutella</ListGroupItemHeading>
+                          <ListGroupItemText>Try Nicciolata Organic Hazelnut Spread with Cocoa & Milk; this product is also GMO Free</ListGroupItemText>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                          <ListGroupItemHeading>Clif Bar</ListGroupItemHeading>
+                          <ListGroupItemText>Try Health Warriors Chia Bar; this product is 100 calories and has 3g of sugar</ListGroupItemText>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                          <ListGroupItemHeading>JIF Peanut Butter</ListGroupItemHeading>
+                          <ListGroupItemText>Try Wild Friends Foods Chocolate Coconut Peanut Butter; this product is kosher and gluten-free</ListGroupItemText>
+                        </ListGroupItem>
+                      </ListGroup>
+                    </Container>
                   </Col>
                 </Row>
               </Container>
             </ParallaxLayer>
 
+            {/* Slide Six */}
             <ParallaxLayer offset={5} speed={0.1}>
-              <Container fluid='true'>
+              <Container fluid={true}>
                 <Row>
                   <Col md='6' className='split-vertical pink-background text-white d-flex align-items-center'>
                     <h1>What can you do to help?</h1>
@@ -116,25 +139,6 @@ class Nutella extends Component {
             </ParallaxLayer>
 
     
-            <ParallaxLayer
-              offset={0}
-              speed={0.1}
-              onClick={() => this.parallax.scrollTo(1)}
-              style={{ display: 'flex', alignItems: 'center' }}>
-              <Container fluid='true'>
-                <Row>
-                  <Col xs='12' className='split-horizontal pink-background d-flex justify-content-center align-items-end'>
-                    <h1 className='text-white pb-3'>Nutella</h1>
-                  </Col>
-                {/* </Row>
-                <Row> */}
-                  <Col xs='12' className='split-horizontal d-flex justify-content-center'>
-                    <h3 className='pt-5'>Know Your Impact</h3>
-                  </Col>
-                </Row>
-              </Container>
-              {/* <div style={{ backgroundColor: '#ffffff', width: '100%', height: '50%', position: 'relative', bottom: '0' }}></div> */}
-            </ParallaxLayer>
     
             <ParallaxLayer
               offset={1}
